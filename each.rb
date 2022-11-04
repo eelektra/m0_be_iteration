34f47deddc81c2811266cc9eef3f4980ad991143
Carol Rieping <carol.rieping@gmail.com>
2022-11-03 20:37:01 -0600

    Fix iteration code

--- each.rb
+++ each.rb
@@ -14,7 +14,16 @@ end
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 words.each do |word|
-  puts "#{words.length == 3}"
+  if word.length == 3
+    p "#{word}"
+  end
 end
-#This code does not work.  It returns false 7 times.
+
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
+
+names = ["Pilar", "Peach", "Pamela", "Tan", "Amanda", "Phil"]
+names.each do |name|
+  if name.index('P') == 0
+    p "#{name}"
+  end
+end
